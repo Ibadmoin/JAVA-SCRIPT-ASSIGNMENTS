8421f62793208ec51099f419517d7e0ed5458cfc
Ibad Moin <123115847+Ibadmoin@users.noreply.github.com>
2023-01-25 21:56:36 +0500

    Add files via upload

---
+++ Assignment#01/alerts.js
@@ -0,0 +1,17 @@
+//1.Write a script to greet your website visitor using JS alert box.
+alert("Welcome visitor");
+//2.Write a script to display following message on your web page.
+alert("Error! Please enter a valid password.");
+//3. Write a script to display following message on your web page: (Hint : Use line break)
+var text= "Welcome to js land.. \n Happy coding!"
+alert(text);
+//4.Write a script to display following messages in sequence:
+alert("Welcome to js land...");
+alert("Happy Coding!");
+//4.Generate the following message through browser’s developer console:
+//step 1 Open console by pressing F12 or inspecting page
+//step 2 In console type alert("Any Message");
+//step 3 Press Enter on your Keyboard.
+
+//---JS ASSIGNMENT #01---//
+//---BY Ibad Moin---//
