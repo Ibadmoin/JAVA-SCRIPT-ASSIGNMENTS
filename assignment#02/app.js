8e352e770759109bfb66e9a980df37108c8d1334
Ibad Moin <123115847+Ibadmoin@users.noreply.github.com>
2023-01-29 23:56:48 +0500

    assignments#02

---
+++ assignment#02/app.js
@@ -0,0 +1,104 @@
+var username = "Ibad Moin"
+alert(username)
+
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+var message = "Hello World"
+alert(message);
+
+
+
+
+// 5. Write a script to display the following alert using one JS variable:
+var pizza = ("pizza \n pizz \n piz \n pi \n p")
+alert(pizza);
+
+
+//6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+var email = "ibadmoin@gmail.com";
+alert("My Email Address is " + email);
+
+
+
+//7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn with book " + book);
+
+// 8. Write a script to display this in browser through JS
+document.write("<h3>Yay! I can write in HTML throught JAVASCRIPT</h3>")
+
+
+
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+
+var symbols = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(symbols);
+
+
+
+
+
+//-------------------------VARIABLES FOR NUMBERS----------------------------------//
+
+//1. Declare a variable called age & assign to it your age. Show
+//your age in an alert box.
+var age = "18";
+alert("I am " + age + " years old")
+
+
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+
+
+
+
+
+
+
+//3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var birthyear = "2004"
+document.write("My birthyear is " + birthyear ) 
+
+
+
+
+
+
+
+//4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+
+
+
+
+
+
+
+//---------------------------------------VARIABLE NAMES: LEGAL & ILLEGAL------------------//
