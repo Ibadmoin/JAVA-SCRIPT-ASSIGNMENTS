d83257e8f1985a68d77ff8f01ff948f64b2ea6d2
Ibad Moin <123115847+Ibadmoin@users.noreply.github.com>
2023-02-14 19:33:49 +0500

    Add files via upload

---
+++ assignment#05/app.js
@@ -0,0 +1,107 @@
+
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+// var students = ["ibad","habib","Abdullah"];
+// console.log(students);
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+// var students = Object.values("iba");
+
+// 3. Declare and initialize a strings array.
+// var text = ["cat","cow","mouse"];
+
+// 4. Declare and initialize a numbers array.
+
+// var num = ["1","2","3","4"];
+
+// 5. Declare and initialize a boolean array.
+
+// var boolean = [true, false];
+
+
+// 6. Declare and initialize a mixed array.
+
+// var array = ["ibad","1",true,];
+
+
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+// var qualifications = ["SSC","HSC","BCS","BS","BCOM","MS","M. Phill","PhD"];
+// document.write("Qualifications :"+"<br/>"+"1) "+ qualifications[0]+"<br/>"+"2) "+ qualifications[1]+"<br/>"+"3) "+ qualifications[2]+"<br/>"+"4) "+ qualifications[3]+"<br/>"+"5) "+ qualifications[4]+"<br/>"+"6) "+ qualifications[5]+"<br/>"+"7) "+ qualifications[6]+"<br/>"+"8) "+ qualifications[7]);
+
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+// var std = ["Micheal","Jhon","Tony"];
+// var marks = ["320","230","480"];
+// var total = 500;
+// document.write("Score of "+ std[0] + " is "+ marks[0]+".Percentage: "+ marks[0]/total*100+"%"+"<br/>"+"Score of "+ std[1] + " is "+ marks[1]+".Percentage: "+ marks[1]/total*100+"%"+"<br/>"+"Score of "+ std[2] + " is "+ marks[2]+".Percentage: "+ marks[2]/total*100+"%"+"<br/>");
+
+
+
+// 9. Initialize an array with color names. Display the array
+// elements in your browser.
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end
+// & add that color to the end of the array. Display the
+// updated array in your browser.
+// c. Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then
+
+
+// remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+
+// 10. Write a program to store student scores in an array &
+// sort the array in ascending order using Array’s sort
+// method.
+
+
+// var scores =[10,20,50,70,80];
+// scores.sort( function( a,b ) {
+//     return a - b;
+// }
+
+// )
+// document.write(scores);
+
+
+// 11. Write a program to initialize an array with city names.
+// Copy 3 array elements from cities array to selectedCities
+// array.
+
+// var city = ["Karachi","Lahore","Quetta","Islamabad","Peshawar"];
+// var selectedCities = city.slice(2,4);
+// document.write("Cities: "+"<br/>"+ city+"<br/>"+ "Selected Cities: " +"<br/>"+ selectedCities);
+
+
+
+// 12. Write a program to create a single string from the
+// below mentioned array:
+// var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+// (Use array’s join method)
+
+
+// var arr = ["This ", " is ", " my ", " cat"];
+// var result = arr.join('');
+// console.log(result);
